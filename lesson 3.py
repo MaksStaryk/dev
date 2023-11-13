ee29fdb37165e554f4e4218205dc0f6534cd8714
Starik <149662909+MaksStaryk@users.noreply.github.com>
2023-11-13 06:01:52 +0300

    Update lesson 3.py last task

--- lesson 3.py
+++ lesson 3.py
@@ -69,6 +69,7 @@ print(school_classes['1a'])
 #print()
 #"task 10в"
 #del school_classes['2a']
-#block = school_classes.values[9]
-#print( sum(block))
+#block = school_classes.get[9]
+#block1 = block.values()
+#print( sum(block1))
 
