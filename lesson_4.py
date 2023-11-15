e3faa0c39ba8610daa67fb132788c909028852ed
Starik <149662909+MaksStaryk@users.noreply.github.com>
2023-11-15 06:09:27 +0300

    lesson 4 task 1 is finished

---
+++ lesson_4.py
@@ -0,0 +1,18 @@
+"""task 1"""
+print("task 1")
+a = 5
+b = 4
+c = 6
+
+if a < 0:
+    print(False)
+if b < 0:
+    print(False)
+if c < 0:
+    print(False)
+else:
+    print(True)
+print()
+"""task 2"""
+print('task 2')
+
