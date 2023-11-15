13b89b03cfd2fe8ed541846f10235dfbd7af1905
Starik <149662909+MaksStaryk@users.noreply.github.com>
2023-11-15 08:43:09 +0300

    Update lesson_4.py

--- lesson_4.py
+++ lesson_4.py
@@ -4,14 +4,10 @@ a = 5
 b = 4
 c = 6
 
-if a < 0:
-    print(False)
-if b < 0:
-    print(False)
-if c < 0:
-    print(False)
-else:
-    print(True)
+a < 0
+ b < 0
+    
+ c < 0:
 print()
 """task 2"""
 print('task 2')
