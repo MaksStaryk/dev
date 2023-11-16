04b7ddc2a63167c504b56963fe9c7471378f9f58
Admin <starikford@gmail.com>
2023-11-16 22:04:41 +0300

    lesson 4 add  task 6

--- lesson4.py
+++ lesson4.py
@@ -5,14 +5,12 @@ a = -1
 b = 21
 c = 3
 
-print( a < 0 )
-print( b < 0)
-print( c < 0)
+print( a < 0 ,b < 0,c < 0)
 print()
 """start task 2"""
 print('start task 2')
-n = 6
-k = 7
+n = 12
+k = 9
 if n //2 *2 ==n and k //2 *2 == k:
     print('есть четность')
 else:
@@ -26,23 +24,23 @@ w = int(v[1])
 if q + w <10:
     print('Da')
 else:
-    print('net')
+    print('Net')
 print('task 5')
 a = 10
 b = 1
 while a <= 29:
-    print(b,'раз:', 10)
+    print(b,'раз:', 10,)
     a += 1
     b += 1
 print()
 ''''task 6'''
 print('task 6')
-some_number_6 =  6
-#i = 1
-#while i <=6:
-    #print((pow(i,3)))
-    #i +=1
-
+some_number_6 = 7
+i = 1
+while i <=some_number_6:
+    print((pow(i,3)), end=' ')
+    i +=1
+print()
 """start task 9"""
 print('start task 9')
 a = 96
