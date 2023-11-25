75bc4adcfcfb05661c2dd49eaa6afd59e5a9bba8
Admin <starikford@gmail.com>
2023-11-25 12:28:14 +0300

    посмотреть как сделать срез

---
+++ lesson_5_task_5.py
@@ -0,0 +1,22 @@
+my_spisok = [5,2,4,5,1,2]
+a = 0
+b = 0
+c = 0
+d = 0
+for x in my_spisok:
+    if x == 5:
+        a += 1
+    elif x == 2:
+        b += 1
+    elif x == 4:
+        c += 1
+    else:
+        d +=1
+print("5:",a,'\n2:',b,'\n4:',c,'\n1:',d )
+
+print()
+print('task6')
+my_list = [741]
+for x in my_list:
+    print(x)
+x = str(x)
